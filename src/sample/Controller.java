1d0bc4e5de2588af8a1582770a583e317f830923
trutina <ericedgari@hotmail.com>
2020-12-25 17:06:01 +0700

    added split pane and possible change window from transaction to sorting center , vice versa

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class Controller {
+    @FXML
+    public BorderPane mainPane;
+    public void transactionWindowButton(ActionEvent event) throws IOException {
+        System.out.println("transaction window");
+        Pane view = FXMLLoader.load(getClass().getResource("Transaction.fxml"));
+        mainPane.setCenter(view);
+    }
+    public void sortingCenterWindowButton(ActionEvent event) throws IOException {
+        System.out.println("clicking sorting center button");
+        Pane view = FXMLLoader.load(getClass().getResource("SortingCenter.fxml"));
+        mainPane.setCenter(view);
+    }
 }
---
+++ src/sample/Transaction.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Transaction {
+
+    public void button2(ActionEvent event){
+        System.out.println("clicking 1 1 1 1");
+    }
+}
